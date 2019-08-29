430ef72a817dc1648ea08a8af1c616884be7e09d
danny crasto <danwald79@gmail.com>
2019-08-29 06:28:10 +0400

    [WIP] initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from pkg_resources import resource_string
+import argparse
+import sys
+
+from pickem.data_normalizer import get_normed_data
+
+def main():
+    ap = argparse.ArgumentParser(description='Parse input game stats and output rank orderd picks')
+    ap.add_argument('infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
+    ap.add_argument('outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout)
+
+    args = ap.parse_args()
+    data = get_normed_data(args['infile'])
+    if data:
+        args['outfile'].write(data)
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ pickem/data_normalizer.py
@@ -0,0 +1,40 @@
+import abc
+from abc import ABC
+import json
+
+
+class DataNormalizer(ABC):
+    @abc.abstractmethod
+    def normalize_data(self):
+        pass
+
+
+class MirrorData(DataNormalizer):
+    def __init__(self, infile_stream):
+        self.infile_stream = infile_stream
+        self.data = None
+
+    def normalize_data(self):
+        if not self.data:
+            data = json.loads(self.infile_stream)
+        return data
+
+
+class OddsAPIData(MirrorData):
+    # Uses stats from https://the-odds-api.com/
+    pass
+
+
+DATA_SOURCES = (MirrorData, OddsAPIData,)
+
+
+def get_normed_data(infile_stream):
+    for src in DATA_SOURCES:
+        try:
+            infile_stream.seek(0)
+            return src(infile_stream).normalize_data()
+        except Exception:
+            pass
+    else:
+        print('Didn\'t parse')
+        return None
