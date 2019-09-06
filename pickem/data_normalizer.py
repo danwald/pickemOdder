ffe2971da72f0c3f5d0594d3722f2ea53ebee305
danny crasto <danwald79@gmail.com>
2019-09-06 21:46:06 +0400

    fixes tabs

--- pickem/data_normalizer.py
+++ pickem/data_normalizer.py
@@ -6,8 +6,7 @@ import datetime
 class DataNormalizer(ABC):
     @abstractmethod
     def normalize_data(self):
-    '''Trys all the data sources and returns a generator in csv of
-       SPORT, EVENT_TIME, HOME-TEAM, AWAY-TEAM, HOME-WIN, AWAY-WIN, BOOKY, UPDATED'''
+    """Trys all the data sources and returns a generator in csv of SPORT, EVENT_TIME, HOME-TEAM, AWAY-TEAM, HOME-WIN, AWAY-WIN, BOOKY, UPDATED"""
         pass
 
 
@@ -28,31 +27,28 @@ class OddsAPIData(MirrorData):
         super(infile_stream):
 
     def normalize_data(self, infile_stream):
-        super().normalize_data()
-		csv = []
+        super().normalize_data(infile_stream)
+        csv = []
         self.data = self.data['data']
         for game in self.data:
-			sport = game['sport_key']
+            sport = game['sport_key']
             home, away = game['teams'][home_index], game['teams'][away_index]
-			kick_off = datetime.datetime.fromtimestamp(game['commence_time'])
+            kick_off = datetime.datetime.fromtimestamp(game['commence_time'])
             for booky in game['sites']:
-				site = booky['site_key']
-				home_win = 1/booky['odds']['h2h'][home_index]*100
-				away_win = 1/booky['odds']['h2h'][away_index]*100
-				updated = datetime.datetime.fromtimestamp(booky['last_update'])
-				csv.append([ sport, kick_off, home, away, home_win, away_win, site, updated])
-		return csv
-
-
-
+                site = booky['site_key']
+                home_win = 1/booky['odds']['h2h'][home_index]*100
+                away_win = 1/booky['odds']['h2h'][away_index]*100
+                updated = datetime.datetime.fromtimestamp(booky['last_update'])
+                csv.append([ sport, kick_off, home, away, home_win, away_win, site, updated])
+                return csv
 
     def get_team_indices(teams, home_team):
-		for index, team in enumerate(teams):
-			if team == home_team:
-				home = index
-			else
-				away = index
-		return home, away
+        for index, team in enumerate(teams):
+            if team == home_team:
+                home = index
+            else:
+                away = index
+        return home, away
 
 
 
