51b7c5ca47bbeb1c0440b82b684e643cf531d168
danny crasto <danwald79@gmail.com>
2019-09-18 23:37:01 +0400

    adding cookiecutters files with setup.py

---
+++ tests/test_PM.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `pickemOdder` package."""
+
+
+import unittest
+
+from pickemOdder import pickemOdder
+
+
+class TestPm(unittest.TestCase):
+    """Tests for `pickemOdder` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
