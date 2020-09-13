259b80e37ba3f2124cfd64cdf8e22c7e6c215ac5
danny crasto <danwald79@gmail.com>
2020-09-13 21:50:49 +0400

    fixes entrypoint and add py3.8 support

--- setup.py
+++ setup.py
@@ -20,22 +20,22 @@ test_requirements = [ ]
 setup(
     author="danny crasto",
     author_email='danwald79@gmail.com',
-    python_requires='>=3.5',
+    python_requires='>=3.6',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     description="Parse input game stats and output rank orderd picks",
     entry_points={
         'console_scripts': [
             'pickemOdder=pickemOdder.cli:main',
-            'getOddsData=pickemOdder.get-odds-data:main',
+            'getOddsData=pickemOdder.get_odds_data:main',
         ],
     },
     install_requires=requirements,
